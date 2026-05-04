1d7db777c2929e40550ad55139b4d6453f129746
Brice404 <Amenle.Foumane@outlook.com>
2026-05-04 02:09:20 -0400

    fixed formatting

--- testKokoro.py
+++ testKokoro.py
@@ -4,12 +4,7 @@ import sounddevice as sd
 kokoro = Kokoro("kokoro-v1.0.onnx", "voices-v1.0.bin")
 
 def speak(text, voice="af_heart", speed=1.0):
-    samples, sample_rate = kokoro.create(
-        text,
-        voice=voice,
-        speed=speed,
-        lang="en-us"
-    )
+    samples, sample_rate = kokoro.create(text, voice=voice, speed=speed,lang="en-us")
     sd.play(samples, sample_rate)
     sd.wait()
 
