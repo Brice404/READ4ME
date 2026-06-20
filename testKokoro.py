73f4275e85d1a9bccf21291eeeea9461bcc288f2
Brice404 <Amenle.Foumane@outlook.com>
2026-06-20 13:02:16 -0400

    don't rmr what I changed lowkey 💀🙏

--- testKokoro.py
+++ testKokoro.py
@@ -7,7 +7,7 @@ def load_model():
     global kokoro
     kokoro = Kokoro("kokoro-v1.0.onnx", "voices-v1.0.bin")
 
-def speak(text, voice="af_heart", speed=1.0):
+def speak(text, voice="am_adam", speed=1.0):
     samples, sample_rate = kokoro.create(text, voice=voice, speed=speed,lang="en-us")
     sd.play(samples, sample_rate)
     sd.wait()
